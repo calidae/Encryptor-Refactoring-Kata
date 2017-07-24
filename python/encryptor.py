fd02427516a78cef674b99a2ae21d4382a7015d6
Sala de Reunions <info@calidae.com>
2017-07-24 22:30:39 +0200

    refactor

--- python/encryptor.py
+++ python/encryptor.py
@@ -1,44 +1,33 @@
+def assert_word(func):
+    def func_wrapper(self, word, *args, **kwargs):
+        if " " in word:
+            raise ValueError()
+        return func(self, word, *args, **kwargs)
+    return func_wrapper
+
+
 class Encryptor:
+    _CESAR_OFFSET = 2
+
+    @assert_word
     def crypt_word(self, word):
-        if " " in word:
-            raise ValueError()
+        return self._crypt_string(word)
 
-        new_word = ""
-        for i in range(len(word)):
-            char_value = ord(word[i]) #charcode
-            new_word += chr( char_value + 2) # ""encripta""
-    
-        return new_word
-
+    @assert_word
     def crypt_word_to_numbers(self, word):
-        if " " in word:
-            raise ValueError()
+        return self._crypt_string(word, str)
 
-        new_word = ""
-        for i in range(len(word)):
-            char_value = ord(word[i])
-            new_word += str(char_value + 2)
-
-        return new_word
-
+    @assert_word
     def crypt_word_with_chars_to_replace(self, word, chars_to_replace):
-        if " " in word:
-            raise ValueError()
-        result = list(word)
-        for i in range(len(word)):
-            for j in range(len(chars_to_replace)):
-                if chars_to_replace[j] == word[i]:
-                    char_value = ord(word[i])
-                    result[i] = chr( char_value + 2)
-        return "".join(result)
+        return "".join([
+            chr(self._get_encrypted_value(char))
+            if char in chars_to_replace
+            else char
+            for char in word
+        ])
 
     def crypt_sentence(self, sentence):
-        new_word = ""
-        for i in range(len(sentence)):
-            char_value = ord(sentence[i])
-            new_word += chr( char_value + 2)
-
-        return new_word
+        return self._crypt_string(sentence)
 
     def get_words(self, sentence):
         return sentence.split()
@@ -48,3 +37,19 @@ class Encryptor:
         for word in words:
             print("<%s>" % word)
 
+    def _assert_is_word(self, string):
+        if " " in string:
+            raise ValueError()
+
+    def _crypt_string(self, string, transform_fn=chr):
+        return ''.join([
+            transform_fn(char)
+            for char in self._generate_encrypted_chars(string)
+        ])
+
+    def _generate_encrypted_chars(self, string):
+        for char in string:
+            yield self._get_encrypted_value(char)
+
+    def _get_encrypted_value(self, char):
+        return ord(char) + self._CESAR_OFFSET
